dae509d5606bf2c7be345db2e9746c031a1b7cc2
Farid-Abishov <abisovfarid14@gmail.com>
2023-04-01 21:37:47 +0400

    slider add

--- Electro/src/js/electro.js
+++ Electro/src/js/electro.js
@@ -4,3 +4,9 @@ var Department_cntnt=document.querySelector(".department_cntnt");
 Depart_title&&Depart_title.addEventListener("click",()=>{
     Department_cntnt.classList.toggle("passive_cntnt");
 })
+var swiper = new Swiper(".slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
