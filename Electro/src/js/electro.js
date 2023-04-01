a927e37808d6eef8d243213e9c0a593471c1e2ab
Farid-Abishov <abisovfarid14@gmail.com>
2023-04-01 15:57:51 +0400

    add

--- Electro/src/js/electro.js
+++ Electro/src/js/electro.js
@@ -1,1 +1,6 @@
-console.log("salam");
+var Depart_title=document.querySelector(".depart_title");
+var Department_cntnt=document.querySelector(".department_cntnt");
+
+Depart_title&&Depart_title.addEventListener("click",()=>{
+    Department_cntnt.classList.toggle("passive_cntnt");
+})
