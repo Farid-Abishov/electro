8e187d68e4f95efaeb47dce6c505092d1d77c39d
Farid-Abishov <abisovfarid14@gmail.com>
2023-04-02 17:52:07 +0400

    add

--- Electro/src/js/electro.js
+++ Electro/src/js/electro.js
@@ -27,3 +27,20 @@ window.addEventListener("scroll",()=>{
       navfix.classList.remove("active_nav")
    }
 })
+//beastseal slider js
+var swiper = new Swiper(".mySwiper", {
+   spaceBetween: 10,
+   slidesPerView: 4,
+   freeMode: true,
+   watchSlidesProgress: true,
+ });
+ var swiper2 = new Swiper(".mySwiper2", {
+   spaceBetween: 10,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   thumbs: {
+     swiper: swiper,
+   },
+ });
