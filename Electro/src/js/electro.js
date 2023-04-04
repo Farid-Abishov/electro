9e8e5403822b08c5c6f7ed45f469d2a744f1d307
Farid-Abishov <abisovfarid14@gmail.com>
2023-04-05 02:30:57 +0400

    add

--- Electro/src/js/electro.js
+++ Electro/src/js/electro.js
@@ -82,3 +82,17 @@ var Mwiper = new Swiper(".markslider", {
 });
 
 //increment and decrement number
+const plus = document.querySelector('.plus_minus'),
+  minus = document.querySelector('.minus_plus');
+
+let number_el = document.querySelector('.number');
+
+plus.addEventListener('click', () => {
+  let val = parseInt(number_el.innerText);
+  number_el.innerText = ++val;
+});
+
+minus.addEventListener('click', () => {
+  let val = parseInt(number_el.innerText);
+  number_el.innerText = --val;
+});
